6ffb643ed778562633ce61b65ceee62c915df4c2
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-09 14:04:26 +0530

    add code

--- projects/common.sh
+++ projects/common.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+LOGFILE="/tmp/${COMPONENT}.log"
--- projects/frontend.sh
+++ projects/frontend.sh
@@ -3,8 +3,10 @@
 COMPONENT=frontend
 Service=nginx
 
+source PROJECTS/common.sh
+
 echo -e "\e[35m This scripting is for Frontend \e[0m"
 
-echo -e "\e[32m This will install $Service  $COMPONENT the server \e[0m"
+echo -e "\e[32m This will install $Service server on $COMPONENT \e[0m"
 
 
