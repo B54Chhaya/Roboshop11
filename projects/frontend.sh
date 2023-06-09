ee866488af42c2a9f79a35a7ae6ebe0cb75f258d
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-09 20:35:42 +0530

    add code

--- projects/frontend.sh
+++ projects/frontend.sh
@@ -4,11 +4,20 @@ source PROJECTS/common.sh
 
 COMPONENT=frontend
 Service=nginx
-
+ID=$(id -u)
 echo -e "\e[35m This scripting is for Frontend \e[0m"
+
+echo "To check whether its user is root or not "
+
+if( $ID -ne 0) ; then   
+echo -m "This script will be excuted by Root user"
+exit 1
+fi
 
 echo -e "\e[32m This will install $Service server on $COMPONENT \e[0m"
 
 echo -n "Installing $Service :"
-sudo yum install nginx -y
+
+sudo yum install nginx -y 
 stat $?
+
