4e40332d8839881d1f5a9293f7b350d4fec50321
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-09 20:20:56 +0530

    add code

--- projects/frontend.sh
+++ projects/frontend.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+source PROJECTS/common.sh
+
 COMPONENT=frontend
 Service=nginx
-
-source PROJECTS/common.sh
 
 echo -e "\e[35m This scripting is for Frontend \e[0m"
 
 echo -e "\e[32m This will install $Service server on $COMPONENT \e[0m"
 
 echo -n "Installing $Service :"
-sudo yum install nginx -y &>> $LOGFILE
+sudo yum install nginx -y &>> LOGFILE
 stat $?
