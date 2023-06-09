1438ca060c64c2cc4e5d0c0e7b4033117aae41d2
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-09 13:59:45 +0530

    add code

--- projects/frontend.sh
+++ projects/frontend.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+COMPONENT=frontend
+Service=nginx
+
 echo -e "\e[35m This scripting is for Frontend \e[0m"
+
+echo -e "\e[32m This will install $Service  $COMPONENT the server \e[0m"
+
 
