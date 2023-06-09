4f3ac1ab03d157606dc3ad2ed63c09de8525d5cb
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-09 21:05:39 +0530

    add code

--- projects/frontend.sh
+++ projects/frontend.sh
@@ -13,6 +13,8 @@ echo "To check whether user is root or not "
 if [ $ID -ne 0 ] ; then   
 echo -e "\e[31m This script will be excuted by Root user or Privileged user \e[0m"
 exit 1
+else
+echo -e "\e[32m User is Root - $ID \e[0m"
 fi
 
 echo -e "\e[32m This will install $Service server on $COMPONENT server \e[0m"
