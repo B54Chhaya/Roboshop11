716c998b9ec3ca6ea32474538fc1f84998cf8aa8
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-09 20:08:52 +0530

    add code

--- projects/frontend.sh
+++ projects/frontend.sh
@@ -9,4 +9,6 @@ echo -e "\e[35m This scripting is for Frontend \e[0m"
 
 echo -e "\e[32m This will install $Service server on $COMPONENT \e[0m"
 
-
+echo -n "Installing $Service :"
+yum install nginx -y &>> $LOGFILE
+stat $?
