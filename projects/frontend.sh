4085984b9153b3d22575ff02c7f45c9982efb30a
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-09 20:58:58 +0530

    add code

--- projects/frontend.sh
+++ projects/frontend.sh
@@ -21,4 +21,9 @@ echo -n "Installing $Service :"
 
 yum install nginx -y &>> "/tmp/${COMPONENT}.log"
 stat $?
+if [ $? eq 0 ] ; then
+   echo -e "\e[33m success \e[0m"
+else   
+   echo -e  "\e[31m failure \e[0m"
+fi
 
