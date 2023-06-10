bbb1c1dc5ac1445bdf6add11215df081b8007a42
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-10 12:51:38 +0530

    Add code

--- projects/frontend.sh
+++ projects/frontend.sh
@@ -6,6 +6,15 @@ COMPONENT=frontend
 Service=nginx
 ID=$(id -u)
 
+# Status function to check wheather it success or failure
+stat()  {
+         if [ $? -eq 0 ] ; then
+            echo -e "\e[33m success \e[0m"
+         else   
+          echo -e  "\e[31m failure \e[0m"
+          exit 2
+         fi
+}
 
 
 echo -e "\e[35m This scripting is for Frontend \e[0m"
