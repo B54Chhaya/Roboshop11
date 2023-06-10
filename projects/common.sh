166b24259d319d9e49a92f3dd0412d1d3e363c12
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-10 12:50:53 +0530

    Add code

--- projects/common.sh
+++ projects/common.sh
@@ -8,6 +8,7 @@ stat()  {
             echo -e "\e[33m success \e[0m"
          else   
           echo -e  "\e[31m failure \e[0m"
+          exit 2
          fi
 }
 
--- projects/frontend.sh
+++ projects/frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source PROJECTS/common.sh
+source projects/common.sh
 
 COMPONENT=frontend
 Service=nginx
